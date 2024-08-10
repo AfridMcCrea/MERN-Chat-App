48906fac96efc6fc07dcd4dbe06f0eaef3afcfa2
AfridMcCrea <73422624+AfridMcCrea@users.noreply.github.com>
2024-08-10 23:05:41 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button } from "./components/ui/button";
+
+export const App = () => {
+  return (
+    <div>
+      <Button>Add a mother fucker bitchhh</Button>
+    </div>
+  );
+};
+export default App;
