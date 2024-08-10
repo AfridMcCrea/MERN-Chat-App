cedec7f85b548e08e98d5b1de03066a0f4a483f9
AfridMcCrea <73422624+AfridMcCrea@users.noreply.github.com>
2024-08-10 23:12:51 +0530

    routing setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import { Button } from "./components/ui/button";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Auth from "./pages/auth/auth";
+import Chat from "./pages/chat/chat";
+import Profile from "./pages/profile/Profile";
 
 export const App = () => {
   return (
-    <div>
-      <Button>Add a mother fucker bitchhh</Button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/auth" element={<Auth/>}/>
+        <Route path="/chat" element={<Chat/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="*" element={<Navigate to="/auth"/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 };
 export default App;
